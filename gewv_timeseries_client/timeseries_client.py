2dca8abe02f5113acb37588c38d46621dd307cfc
paul20180817 <75123024+paul20180817@users.noreply.github.com>
2021-07-06 14:00:50 +0200

    Query dataframe (#4)
    * add get_dataframe_timeindex
    
    * clear up get_dataframe with timeindex
    
    * feature(main): add query_dataframe with datetimeindex
    
    * rework of functions get_dataframe/query_dataframe

--- gewv_timeseries_client/timeseries_client.py
+++ gewv_timeseries_client/timeseries_client.py
@@ -137,12 +137,24 @@ class TimeseriesClient:
         return tables
 
     def get_dataframe(self, **kwargs):
+
+        return self.query_dataframe(query=self.build_query(**kwargs))
+
+    def query_dataframe(
+        self,
+        flux_query: str,
+    ):
+        """
+        with this function you can send a own query to InfluxDB and
+        you will get back a dataframe with datetimeindex
+        """
+
         if not self.health:
             raise Exception("Influx DB is not reachable or unhealthy.")
 
         df = cast(
             DataFrame,
-            self._query_api.query_data_frame(query=self.build_query(**kwargs)),
+            self._query_api.query_data_frame(query=flux_query),
         )
 
         if "_time" in df.columns:
